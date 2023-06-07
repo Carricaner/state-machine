0fcf4e1879cfee3f9c74c025815039b5990e3a7d
Darren <carricanbuild@gmail.com>
2023-06-07 17:09:05 +0800

    tidy up things

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,22 @@ plugins {
     id("java")
     id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("maven-publish")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+val myGroupId = "com.example"
+val myArtifactId = "state-machine"
+val myVersion = "1.0.0"
+
+group = myGroupId
+version = myVersion
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
@@ -22,3 +26,20 @@ dependencies {
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
+
+// publishing is not working now... :(
+//publishing {
+//    publications {
+//        create<MavenPublication>("mavenJava") {
+//            from(components["java"])
+//            groupId = myGroupId
+//            artifactId = myArtifactId
+//            version = myVersion
+//        }
+//    }
+//    repositories {
+//        maven {
+//            url = uri("${System.getProperty("user.home")}/.m2/repository")
+//        }
+//    }
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
-rootProject.name = "statemachine"
-
+rootProject.name = "state-machine"
